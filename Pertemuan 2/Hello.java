6edcc2354a58f687c8dc534ecba4221d8b1c8664
Fauzan Najib Habiibii <fauzannajib.2021@student.uny.ac.id>
2021-09-15 22:12:30 +0700

    Menambahkan komentar

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,5 +1,10 @@
+/*
+Nama: Fauzan Najib H
+NIM: 21537144038
+*/
 public class Hello {
     public static void main(String[] args){
+        //Untuk menambahkan teks
         System.out.println("Hello, World!");
     }
 }
