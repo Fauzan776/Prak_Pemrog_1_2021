680765d5c71d8bbc10799af9243900c5d9037110
Fauzan Najib Habiibii <fauzannajib.2021@student.uny.ac.id>
2021-09-16 15:16:05 +0700

    Tugas pertemuan 3

---
+++ Pertemuan 3/Identitas.java
@@ -0,0 +1,20 @@
+public class Identitas {
+    public static void main(String[] args){
+        String namaLengkap = "Fauzan Najib Habiibii";
+        String namaPanggilan = "Fauzan";
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "Pendidikan Teknik Elektronika dan Informatika";
+        String fakultas = "Teknik";
+        String nim = "21537144038";
+        double angkatan = 2021;
+        double ipk = 3.7;
+        System.out.println("Nama saya adalah " + namaLengkap);
+        System.out.println("Biasa dipanggil " + namaPanggilan);
+        System.out.println("Nim saya " + nim);
+        System.out.println("Angkatan " + (int) angkatan);
+        System.out.println("Program Studi " + programStudi);
+        System.out.println("Jurusan " + jurusan);
+        System.out.println("Fakultas " + fakultas);
+        System.out.println("IPK Harapan " + ipk);
+    }
+}
---
+++ Pertemuan 3/identitas.py
@@ -0,0 +1,16 @@
+namaLengkap = "Fauzan Najib Habiibii"
+namaPanggilan = "Fauzan"
+programStudi = "Teknologi Informasi"
+jurusan = "Pendidikan Teknik Elektronika dan Informatika"
+fakultas = "Teknik"
+nim = 21537144038
+angkatan = 2021
+ipk = 3.7
+print("Nama saya adalah " + namaLengkap)
+print("Biasa dipanggil " + namaPanggilan)
+print("Nim saya " + str(nim))
+print("Angkatan " + str(angkatan))
+print("Program Studi " + programStudi)
+print("Jurusan " + jurusan)
+print("Fakultas " + fakultas)
+print("IPK Harapan " + str(ipk))
