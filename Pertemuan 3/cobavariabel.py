dc99aed9bec0fd21390427dda99a576b50447784
Fauzan Najib Habiibii <fauzannajib.2021@student.uny.ac.id>
2021-09-16 14:24:43 +0700

    Belajar tipe data di python

---
+++ Pertemuan 3/cobavariabel.py
@@ -0,0 +1,9 @@
+namaDepan = "Fauzan"
+umur = 18
+tinggiBadan = 1.7
+print("Nama saya adalah " + namaDepan)
+print("Umur saya " + str(umur) + " tahun")
+print("Tinggi badan saya " + str(tinggiBadan) + "m")
+print(type(namaDepan))
+print(type(umur))
+print(type(tinggiBadan))
